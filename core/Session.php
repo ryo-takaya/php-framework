e00f94a9ad621a7458ba60e533a3c5b743f91b18
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2022-03-06 19:15:50 +0900

    Sessionクラスの作成

---
+++ core/Session.php
@@ -0,0 +1,61 @@
+<?php
+
+class Session
+{
+    protected static $sessionStarted = false;
+    protected static $sessionIdRegenerated = false;
+
+    public function __construct()
+    {
+        if (!self::$sessionStarted) {
+            session_start();
+
+            self::$sessionStarted = true;
+        }
+    }
+
+    public function set(string $name, string $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    public function get(string $name, $default = null)
+    {
+        if (isset($_SESSION[$name])) {
+            return $_SESSION[$name];
+        }
+
+        return $default;
+    }
+
+    public function remove($name)
+    {
+        unset($_SESSION[$name]);
+    }
+
+    public function clear()
+    {
+        $_SESSION = [];
+    }
+
+    public function regenerate($destroy = true)
+    {
+        if (!self::$sessionIdRegenerated) {
+            session_regenerate_id($destroy);
+
+            self::$sessionIdRegenerated = true;
+        }
+    }
+
+    public function setAuthenticated(bool $bool)
+    {
+        $this->set('_authenticated', $bool);
+
+        $this->regenerate();
+    }
+
+    public function isAuthenticated()
+    {
+        return $this->get('_authenticated', false);
+    }
+}
