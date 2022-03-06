d975ef56b6da9703b0cc9bd2bc468301ff2117b8
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2022-03-06 12:02:54 +0900

    Responseクラスを作成

---
+++ core/Response.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core;
+
+class Response {
+    protected $content;
+    protected $statusCode = '200';
+    protected $statusText = 'OK';
+    protected $httpHeaders = [];
+
+    public function send(){
+        header('HTTP1.1' . $this->statusCode. ' '. $this->statusText);
+        foreach ($this->httpHeaders as $name => $value) {
+            header($name. ': ' .$value);
+        }
+
+        echo $this->content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function setStatusCode($statusCode, $statusText = '')
+    {
+        $this->statusCode = $statusCode;
+        $this->statusText = $statusText;
+    }
+
+    public function setHttpHeader($name, $value)
+    {
+        $this->httpHeaders[$name] = $value;
+    }
+}
